9f09b7716776e37d5a894699484877bd47a70174
ThijsO26 <TF.Orsel@student.han.nl>
2021-02-26 11:35:54 +0100

    initial commit

---
+++ src/main/java/nl/han/dea/rest/App.java
@@ -0,0 +1,13 @@
+package nl.han.dea.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
